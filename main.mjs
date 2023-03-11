14731d4e5d106ded7e987972f42d9d1baa6415b8
tiye <jiyinyiyong@gmail.com>
2023-03-11 17:54:28 +0800

    add querystring field; tag 0.0.15

--- main.mjs
+++ main.mjs
@@ -3,8 +3,8 @@ import { main_$x_ } from "./js-out/skir.app.main.mjs"
 
 main_$x_()
 
-if (import.meta.hot) {
-  import.meta.hot.accept('./js-out/skir.app.main.mjs', (main) => {
-    main.reload_$x_()
-  })
-}
+// if (import.meta.hot) {
+//   import.meta.hot.accept('./js-out/skir.app.main.mjs', (main) => {
+//     main.reload_$x_()
+//   })
+// }
